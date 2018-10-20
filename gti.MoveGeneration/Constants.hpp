bf243ae0124b5039d1fc303edd1293e43432f253
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-10-20 20:44:22 +0300

    Declared Constants.

---
+++ gti.MoveGeneration/Constants.hpp
@@ -0,0 +1,19 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+#include <windows.h>
+
+#include "Typedefs.hpp"
+
+class Constants
+{
+public:
+	static constexpr WORD CONSOLE_RED_COLOR = 4;
+	static constexpr WORD CONSOLE_BLUE_COLOR = 1;
+	static constexpr WORD CONSOLE_GREEN_COLOR = 2;
+	static constexpr WORD CONSOLE_YELLOW_COLOR = 14;
+	static constexpr WORD CONSOLE_WHITE_COLOR = 15;
+	static constexpr WORD CONSOLE_GRAY_COLOR = 8;
+};
+
+#endif // !CONSTANTS_HPP
