831872676f582e884b987216a88f4f5908ed2ce7
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-10-21 13:40:34 +0300

    Added MoveGeneratorTests.

---
+++ gti.Tests/MoveGeneratorTests.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "../gti/Board.hpp"
+#include "../gti/Board.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace gtiTests
+{
+	TEST_CLASS(MoveGeneratorTests)
+	{
+	public:
+		TEST_METHOD(TODO)
+		{
+		}
+	};
+}
