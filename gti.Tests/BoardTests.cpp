0daf02b7b236249f88fabec0a209be3b0bc9a30e
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-10-21 13:58:26 +0300

    Added tests for GetRed/Blue/Yellow/Green().

--- gti.Tests/BoardTests.cpp
+++ gti.Tests/BoardTests.cpp
@@ -2,7 +2,6 @@
 #include "CppUnitTest.h"
 
 #include "../gti/Board.hpp"
-#include "../gti/Board.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -18,6 +17,42 @@ namespace gtiTests
 			I256 expectedEmptySquares{ "46591068453637164260361794096484197643923981598720" };
 
 			Assert::AreEqual(emptySquares.str(), expectedEmptySquares.str());
+		}
+
+		TEST_METHOD(GetRed_InitialBoard_Red)
+		{
+			Board board{};
+			auto red = board.GetRed();
+			I256 expectedRed{ "12505926868080732774944114433913560689973096351229437542400" };
+
+			Assert::AreEqual(red.str(), expectedRed.str());
+		}
+
+		TEST_METHOD(GetBlue_InitialBoard_Blue)
+		{
+			Board board{};
+			auto blue = board.GetBlue();
+			I256 expectedBlue{ "4181644952077523914472180097270081996718080" };
+
+			Assert::AreEqual(blue.str(), expectedBlue.str());
+		}
+
+		TEST_METHOD(GetYellow_InitialBoard_Yellow)
+		{
+			Board board{};
+			auto yellow = board.GetYellow();
+			I256 expectedYellow{ "33425400" };
+
+			Assert::AreEqual(yellow.str(), expectedYellow.str());
+		}
+
+		TEST_METHOD(GetGreen_InitialBoard_Green)
+		{
+			Board board{};
+			auto green = board.GetGreen();
+			I256 expectedGreen{ "17128017723709537953678049678418255858557255680" };
+
+			Assert::AreEqual(green.str(), expectedGreen.str());
 		}
 	};
 }
