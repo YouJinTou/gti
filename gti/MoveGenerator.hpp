9e1d6e0c1878e1f6ff96030539d2e8daec758d0e
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-10-27 16:06:58 +0300

    Made all Get...Moves() public.

--- gti/MoveGenerator.hpp
+++ gti/MoveGenerator.hpp
@@ -12,6 +12,12 @@ class MoveGenerator
 public:
 	MoveGenerator(const Board& board);
 	std::vector<Move> GetMoves() const;
+	std::vector<Move> GetPawnMoves() const;
+	std::vector<Move> GetKnightMoves() const;
+	std::vector<Move> GetBishopMoves() const;
+	std::vector<Move> GetRookMoves() const;
+	std::vector<Move> GetQueenMoves() const;
+	std::vector<Move> GetKingMoves() const;
 private:
 	static constexpr int RED_PAWN_FORWARD_MOVE_SHIFT = 14;
 	static constexpr int RED_PAWN_LEFT_CAPTURE_SHIFT = 15;
@@ -19,7 +25,6 @@ private:
 private:
 	const Board& board;
 private:
-	std::vector<Move> GetPawnMoves() const;
 	std::vector<Move> GetPawnMoves(
 		I256 pawns,
 		bool increasing,
@@ -31,11 +36,6 @@ private:
 		I256 promotionLine,
 		I256 leftCaptureOpposingLine,
 		I256 rightCaptureOpposingLine) const;
-	std::vector<Move> GetKnightMoves() const;
-	std::vector<Move> GetBishopMoves() const;
-	std::vector<Move> GetRookMoves() const;
-	std::vector<Move> GetQueenMoves() const;
-	std::vector<Move> GetKingMoves() const;
 };
 
 #endif // !MOVE_GENERATOR_HPP
