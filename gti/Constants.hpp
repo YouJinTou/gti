6141dc4ed6e109e5e2f22febb79eb1e0f2f50945
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-11-04 11:09:51 +0200

    Added CastlingRight.

--- gti/Constants.hpp
+++ gti/Constants.hpp
@@ -9,4 +9,21 @@ enum PlayerColor
 	Green
 };
 
+enum CastlingRight
+{
+	RedKingSide = 1,
+	RedQueenSide = 2,
+	BlueKingSide = 4,
+	BlueQueenSide = 8,
+	YellowKingSide = 16,
+	YellowQueenSide = 32,
+	GreenKingSide = 64,
+	GreenQueenSide = 128,
+};
+
+inline CastlingRight operator|(CastlingRight a, CastlingRight b)
+{
+	return static_cast<CastlingRight>(static_cast<int>(a) | static_cast<int>(b));
+}
+
 #endif // !CONSTANTS_HPP
