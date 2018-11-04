3563ea303dd6a1d3a8e3b8e8e7527cb7ced5ba1c
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-11-04 12:14:21 +0200

    Added timer.

--- gti/main.cpp
+++ gti/main.cpp
@@ -1,18 +1,27 @@
+#include "PositionStringParser.hpp"
 #include <iostream>
 #include "Board.hpp"
 #include "BoardDrawer.hpp"
 #include "MoveGenerator.hpp"
 #include "Utils.hpp"
+#include <chrono>
 
-int main() 
+int main()
 {
-	Board board{};
-	Utils::PrintBinary(board.GetEmptySquares());
-	/*BoardDrawer drawer{ board };
-	MoveGenerator generator{ board };
-	auto moves = generator.GetMoves();
+	PositionStringParser parser;
+	Board board = parser.ParsePositionString(PositionStringParser::INITIAL_STRING);
+	auto start = std::chrono::high_resolution_clock::now();
 
-	drawer.DrawBoard();*/
+	for (size_t i = 0; i < 1000; i++)
+	{
+		MoveGenerator generator{ board };
+		auto moves = generator.GetMoves();
+	}
+
+	auto diff = std::chrono::high_resolution_clock::now() - start;
+	auto time = std::chrono::duration_cast<std::chrono::milliseconds>(diff);
+
+	std::cout << time.count() << std::endl;
 
 	std::getchar();
 
