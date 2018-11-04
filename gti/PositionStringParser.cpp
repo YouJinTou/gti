3bee64f6278f05b523c54d47a1ad3e1edc1074eb
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-11-04 10:52:49 +0200

    Added SetPosition().

--- gti/PositionStringParser.cpp
+++ gti/PositionStringParser.cpp
@@ -1,12 +1,232 @@
+#include <numeric>
+#include <boost/algorithm/string.hpp>
+#include <regex>
+
 #include "PositionStringParser.hpp"
 #include "Board.hpp"
+#include "Utils.hpp"
 
-Board PositionStringParser::ParsePositionString(std::string positionString) const
+Board PositionStringParser::ParsePositionString(const std::string& positionString)
 {
-	return Board();
+	SetPosition(positionString);
+
+	return Board(
+		PlayerColor::Red,
+		rPawns,
+		rKnights,
+		rBishops,
+		rRooks,
+		rQueens,
+		rKing,
+		bPawns,
+		bKnights,
+		bBishops,
+		bRooks,
+		bQueens,
+		bKing,
+		yPawns,
+		yKnights,
+		yBishops,
+		yRooks,
+		yQueens,
+		yKing,
+		gPawns,
+		gKnights,
+		gBishops,
+		gRooks,
+		gQueens,
+		gKing
+	);
 }
 
 std::string PositionStringParser::GetPositionString(const Board& board) const
 {
 	return std::string();
+}
+
+void PositionStringParser::SetPosition(const std::string& positionString)
+{
+	if (positionString.empty())
+	{
+		throw;
+	}
+
+	std::vector<std::string> slashTokens;
+
+	boost::split(slashTokens, positionString, [](char c) {return c == '/'; });
+
+	if (slashTokens.size() != SLASH_DELIMITED_TOKENS)
+	{
+		throw;
+	}
+
+	std::vector<std::string> emptySpaceTokens;
+
+	boost::split(emptySpaceTokens, slashTokens.back(), [](char c) {return c == ' '; });
+
+	if (emptySpaceTokens.size() != LAST_TOKEN_TOKENS)
+	{
+		throw;
+	}
+
+	slashTokens.pop_back();
+
+	std::vector<std::string> positionTokens(slashTokens);
+
+	positionTokens.push_back(emptySpaceTokens.front());
+
+	std::regex regex("(\\d+)|(rP)|(rN)|(rB)|(rR)|(rQ)|(rq)|(rK)|(bP)|(bN)|(bB)|(bR)|(bQ)|(bq)|(bK)|(yP)|(yN)|(yB)|(yR)|(yQ)|(yq)|(yK)|(gP)|(gN)|(gB)|(gR)|(gQ)|(gq)|(gK)");
+
+	int position = Board::TOTAL_SQUARES;
+
+	for (auto& token : positionTokens)
+	{
+		std::vector<int> squares;
+		std::vector<std::string> pieces;
+		std::smatch match;
+		std::string matchable(token);
+
+		while (std::regex_search(matchable, match, regex))
+		{
+			std::string matchString = match[0].str();
+			bool isNumber =
+				!matchString.empty() &&
+				std::find_if(
+					matchString.cbegin(),
+					matchString.cend(),
+					[](char c) { return !std::isdigit(c); }) == matchString.cend();
+
+			if (isNumber)
+			{
+				int number = std::stoi(match[0]);
+				position -= number;
+
+				if (number <= 0 || number > Board::TOTAL_SQUARES)
+				{
+					throw;
+				}
+
+				squares.push_back(number);
+			}
+			else
+			{
+				position--;
+
+				SetSquare(matchString, position);
+
+				pieces.push_back(matchString);
+			}
+
+			matchable = match.suffix();
+		}
+
+		int totalOccupancy = std::accumulate(squares.cbegin(), squares.cend(), 0) + pieces.size();
+
+		if (totalOccupancy != Board::SQUARES_PER_SIDE)
+		{
+			throw;
+		}
+	}
+}
+
+void PositionStringParser::SetSquare(const std::string& matchString, int position)
+{
+	auto mask = (I256{ 1 } << position);
+
+	if (matchString == RED_PAWN)
+	{
+		rPawns |= mask;
+	}
+	else if (matchString == RED_KNIGHT)
+	{
+		rKnights |= mask;
+	}
+	else if (matchString == RED_BISHOP)
+	{
+		rBishops |= mask;
+	}
+	else if (matchString == RED_ROOK)
+	{
+		rRooks |= mask;
+	}
+	else if (matchString == RED_QUEEN || matchString == RED_NEW_QUEEN)
+	{
+		rQueens |= mask;
+	}
+	else if (matchString == RED_KING)
+	{
+		rKing |= mask;
+	}
+	else if (matchString == BLUE_PAWN)
+	{
+		bPawns |= mask;
+	}
+	else if (matchString == BLUE_KNIGHT)
+	{
+		bKnights |= mask;
+	}
+	else if (matchString == BLUE_BISHOP)
+	{
+		bBishops |= mask;
+	}
+	else if (matchString == BLUE_ROOK)
+	{
+		bRooks |= mask;
+	}
+	else if (matchString == BLUE_QUEEN || matchString == BLUE_NEW_QUEEN)
+	{
+		bQueens |= mask;
+	}
+	else if (matchString == BLUE_KING)
+	{
+		bKing |= mask;
+	}
+	else if (matchString == YELLOW_PAWN)
+	{
+		yPawns |= mask;
+	}
+	else if (matchString == YELLOW_KNIGHT)
+	{
+		yKnights |= mask;
+	}
+	else if (matchString == YELLOW_BISHOP)
+	{
+		yBishops |= mask;
+	}
+	else if (matchString == YELLOW_ROOK)
+	{
+		yRooks |= mask;
+	}
+	else if (matchString == YELLOW_QUEEN || matchString == YELLOW_NEW_QUEEN)
+	{
+		yQueens |= mask;
+	}
+	else if (matchString == YELLOW_KING)
+	{
+		yKing |= mask;
+	}
+	else if (matchString == GREEN_PAWN)
+	{
+		gPawns |= mask;
+	}
+	else if (matchString == GREEN_KNIGHT)
+	{
+		gKnights |= mask;
+	}
+	else if (matchString == GREEN_BISHOP)
+	{
+		gBishops |= mask;
+	}
+	else if (matchString == GREEN_ROOK)
+	{
+		gRooks |= mask;
+	}
+	else if (matchString == GREEN_QUEEN || matchString == GREEN_NEW_QUEEN)
+	{
+		gQueens |= mask;
+	}
+	else if (matchString == GREEN_KING)
+	{
+		gKing |= mask;
+	}
 }
--- gti/PositionStringParser.hpp
+++ gti/PositionStringParser.hpp
@@ -2,16 +2,87 @@
 #define POSITION_STRING_PARSER_HPP
 
 #include <string>
+#include <vector>
+
+#include "Constants.hpp"
+#include "Typedefs.hpp"
 
 class Board;
 
 class PositionStringParser
 {
 public:
-	const std::string INITIAL_STRING{ "3rRrNrBrQrKrBrNrR3/3rPrPrPrPrPrPrPrP3/14/bRbP10gPgR/bNbP10gPgN/bBbP10gPgB/bQbP10/gPgK/bKbP10gPgQ/bBbP10gPgB/bNbP10gPgN/bRbP10gPgR/14/3yPyPyPyPyPyPyPyP3/3yRrNyByKyQyByNyR3 r rKrQbKbQyKyQgKgQ – 0 1" };
+	static constexpr const char* INITIAL_STRING = "3rRrNrBrQrKrBrNrR3/3rPrPrPrPrPrPrPrP3/14/bRbP10gPgR/bNbP10gPgN/bBbP10gPgB/bQbP10gPgK/bKbP10gPgQ/bBbP10gPgB/bNbP10gPgN/bRbP10gPgR/14/3yPyPyPyPyPyPyPyP3/3yRyNyByKyQyByNyR3 r rKrQbKbQyKyQgKgQ rbyw 0 1";
+	static constexpr const char* RED_PAWN = "rP";
+	static constexpr const char* RED_KNIGHT = "rN";
+	static constexpr const char* RED_BISHOP = "rB";
+	static constexpr const char* RED_ROOK = "rR";
+	static constexpr const char* RED_QUEEN = "rQ";
+	static constexpr const char* RED_NEW_QUEEN = "rq";
+	static constexpr const char* RED_KING = "rK";
+	static constexpr const char* BLUE_PAWN = "bP";
+	static constexpr const char* BLUE_KNIGHT = "bN";
+	static constexpr const char* BLUE_BISHOP = "bB";
+	static constexpr const char* BLUE_ROOK = "bR";
+	static constexpr const char* BLUE_QUEEN = "bQ";
+	static constexpr const char* BLUE_NEW_QUEEN = "bq";
+	static constexpr const char* BLUE_KING = "bK";
+	static constexpr const char* YELLOW_PAWN = "yP";
+	static constexpr const char* YELLOW_KNIGHT = "yN";
+	static constexpr const char* YELLOW_BISHOP = "yB";
+	static constexpr const char* YELLOW_ROOK = "yR";
+	static constexpr const char* YELLOW_QUEEN = "yQ";
+	static constexpr const char* YELLOW_NEW_QUEEN = "yq";
+	static constexpr const char* YELLOW_KING = "yK";
+	static constexpr const char* GREEN_PAWN = "gP";
+	static constexpr const char* GREEN_KNIGHT = "gN";
+	static constexpr const char* GREEN_BISHOP = "gB";
+	static constexpr const char* GREEN_ROOK = "gR";
+	static constexpr const char* GREEN_QUEEN = "gQ";
+	static constexpr const char* GREEN_NEW_QUEEN = "gq";
+	static constexpr const char* GREEN_KING = "gK";
+private:
+	const int SLASH_DELIMITED_TOKENS = 14;
+	const int LAST_TOKEN_TOKENS = 6;
+	const std::vector<std::string> PIECES
+	{ 
+		RED_PAWN, RED_KNIGHT, RED_BISHOP, RED_ROOK, RED_QUEEN, RED_NEW_QUEEN, RED_KING,
+		BLUE_PAWN, BLUE_KNIGHT, BLUE_BISHOP, BLUE_ROOK, BLUE_QUEEN, BLUE_NEW_QUEEN, BLUE_KING,
+		YELLOW_PAWN, YELLOW_KNIGHT, YELLOW_BISHOP, YELLOW_ROOK, YELLOW_QUEEN, YELLOW_NEW_QUEEN, YELLOW_KING,
+		GREEN_PAWN, GREEN_KNIGHT, GREEN_BISHOP, GREEN_ROOK, GREEN_QUEEN, GREEN_NEW_QUEEN, GREEN_KING
+	};
 public:
-	Board ParsePositionString(std::string positionString) const;
+	Board ParsePositionString(const std::string& positionString);
 	std::string GetPositionString(const Board& board) const;
+private:
+	void SetPosition(const std::string& positionString);
+	void SetSquare(const std::string& matchString, int position);
+private:
+	PlayerColor toMove;
+	I256 rPawns;
+	I256 rKnights;
+	I256 rBishops;
+	I256 rRooks;
+	I256 rQueens;
+	I256 rKing;
+	I256 bPawns;
+	I256 bKnights;
+	I256 bBishops;
+	I256 bRooks;
+	I256 bQueens;
+	I256 bKing;
+	I256 yPawns;
+	I256 yKnights;
+	I256 yBishops;
+	I256 yRooks;
+	I256 yQueens;
+	I256 yKing;
+	I256 gPawns;
+	I256 gKnights;
+	I256 gBishops;
+	I256 gRooks;
+	I256 gQueens;
+	I256 gKing;
 };
 
 #endif // !POSITION_STRING_PARSER_HPP
